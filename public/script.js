ebc4b328017416e34796363089b9fdb10ddc4fec
swal0618 <swal0618@uni.sydney.edu.au>
2022-06-02 16:54:39 +1000

    Working Kanban

--- public/script.js
+++ public/script.js
@@ -33,7 +33,7 @@ var pomodoro = {
       this.minutes = mins;
       this.seconds = secs;
       this.started = started;
-      this.fillerIncrement = 200/(this.minutes*60);
+      this.fillerIncrement = 225/(this.minutes*60);
       this.fillerHeight = 0;  
     },
     startWork: function() {
@@ -214,65 +214,4 @@ removeIcon.addEventListener("click", ()=>{
     infoText.style.color = "#9A9A9A";
     infoText.innerHTML = "Type any existing word and press enter to get meaning, example, synonyms, etc.";
 });
-
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.currentTarget.appendChild(document.getElementById(data));
-}
-
-function createTask(){
-    var x = document.getElementById("inprogress");
-    var y = document.getElementById("done");
-    var z = document.getElementById("create-new-task-block");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        y.style.display = "block";
-        z.style.display = "none";
-    } else {
-        x.style.display = "none";
-        y.style.display = "none";
-        z.style.display = "flex";
-    }
-}
-
-function saveTask(){
-    // var saveButton = document.getElementById("save-button");
-    // var editButton = document.getElementById("edit-button");
-    // if (saveButton.style.display === "none") {
-    //     saveButton.style.display = "block";
-    //     editButton.style.display = "none";
-    // } else{
-    //     saveButton.style.display = "none";
-    //     editButton.style.display = "block";
-    // }
-
-    var todo = document.getElementById("todo");
-    var taskName = document.getElementById("task-name").value;
-    todo.innerHTML += `
-    <div class="task" id="${taskName.toLowerCase().split(" ").join("")}" draggable="true" ondragstart="drag(event)">
-        <span>${taskName}</span>
-    </div>
-    `
-}
-
-function editTask(){
-    var saveButton = document.getElementById("save-button");
-    var editButton = document.getElementById("edit-button");
-    if (saveButton.style.display === "none") {
-        saveButton.style.display = "block";
-        editButton.style.display = "none";
-    } else{
-        saveButton.style.display = "none";
-        editButton.style.display = "block";
-    }
-}
-
+  
