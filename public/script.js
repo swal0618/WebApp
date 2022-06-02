2239793e781aa8a5cecbe47e7ce662d71c8d9ef1
swal0618 <swal0618@uni.sydney.edu.au>
2022-06-03 03:07:57 +1000

    Minor Tweaks

--- public/script.js
+++ public/script.js
@@ -1,87 +1,87 @@
 var pomodoro = {
-    started : false,
-    minutes : 0,
-    seconds : 0,
-    fillerHeight : 0,
-    fillerIncrement : 0,
-    interval : null,
-    minutesDom : null,
-    secondsDom : null,
-    fillerDom : null,
-    init : function(){
-      var self = this;
-      this.minutesDom = document.querySelector('#minutes');
-      this.secondsDom = document.querySelector('#seconds');
-      this.fillerDom = document.querySelector('#filler');
-      this.interval = setInterval(function(){
-        self.intervalCallback.apply(self);
-      }, 1000);
-      document.querySelector('#work').onclick = function(){
-        self.startWork.apply(self);
-      };
-      document.querySelector('#shortBreak').onclick = function(){
-        self.startShortBreak.apply(self);
-      };
-      document.querySelector('#longBreak').onclick = function(){
-        self.startLongBreak.apply(self);
-      };
-      document.querySelector('#stop').onclick = function(){
-        self.stopTimer.apply(self);
-      };
+    started: false,
+    minutes: 0,
+    seconds: 0,
+    fillerHeight: 0,
+    fillerIncrement: 0,
+    interval: null,
+    minutesDom: null,
+    secondsDom: null,
+    fillerDom: null,
+    init: function () {
+        var self = this;
+        this.minutesDom = document.querySelector('#minutes');
+        this.secondsDom = document.querySelector('#seconds');
+        this.fillerDom = document.querySelector('#filler');
+        this.interval = setInterval(function () {
+            self.intervalCallback.apply(self);
+        }, 1000);
+        document.querySelector('#work').onclick = function () {
+            self.startWork.apply(self);
+        };
+        document.querySelector('#shortBreak').onclick = function () {
+            self.startShortBreak.apply(self);
+        };
+        document.querySelector('#longBreak').onclick = function () {
+            self.startLongBreak.apply(self);
+        };
+        document.querySelector('#stop').onclick = function () {
+            self.stopTimer.apply(self);
+        };
     },
-    resetVariables : function(mins, secs, started){
-      this.minutes = mins;
-      this.seconds = secs;
-      this.started = started;
-      this.fillerIncrement = 225/(this.minutes*60);
-      this.fillerHeight = 0;  
+    resetVariables: function (mins, secs, started) {
+        this.minutes = mins;
+        this.seconds = secs;
+        this.started = started;
+        this.fillerIncrement = 225 / (this.minutes * 60);
+        this.fillerHeight = 0;
     },
-    startWork: function() {
-      this.resetVariables(25, 0, true);
+    startWork: function () {
+        this.resetVariables(25, 0, true);
     },
-    startShortBreak : function(){
-      this.resetVariables(5, 0, true);
+    startShortBreak: function () {
+        this.resetVariables(5, 0, true);
     },
-    startLongBreak : function(){
-      this.resetVariables(15, 0, true);
+    startLongBreak: function () {
+        this.resetVariables(15, 0, true);
     },
-    stopTimer : function(){
-      this.resetVariables(25, 0, false);
-      this.updateDom();
+    stopTimer: function () {
+        this.resetVariables(25, 0, false);
+        this.updateDom();
     },
-    toDoubleDigit : function(num){
-      if(num < 10) {
-        return "0" + parseInt(num, 10);
-      }
-      return num;
-    },
-    updateDom : function(){
-      this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
-      this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
-      this.fillerHeight = this.fillerHeight + this.fillerIncrement;
-      this.fillerDom.style.height = this.fillerHeight + 'px';
-    },
-    intervalCallback : function(){
-      if(!this.started) return false;
-      if(this.seconds == 0) {
-        if(this.minutes == 0) {
-          this.timerComplete();
-          return;
+    toDoubleDigit: function (num) {
+        if (num < 10) {
+            return "0" + parseInt(num, 10);
         }
-        this.seconds = 59;
-        this.minutes--;
-      } else {
-        this.seconds--;
-      }
-      this.updateDom();
+        return num;
     },
-    timerComplete : function(){
-      this.started = false;
-      this.fillerHeight = 0;
+    updateDom: function () {
+        this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
+        this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
+        this.fillerHeight = this.fillerHeight + this.fillerIncrement;
+        this.fillerDom.style.height = this.fillerHeight + 'px';
+    },
+    intervalCallback: function () {
+        if (!this.started) return false;
+        if (this.seconds == 0) {
+            if (this.minutes == 0) {
+                this.timerComplete();
+                return;
+            }
+            this.seconds = 59;
+            this.minutes--;
+        } else {
+            this.seconds--;
+        }
+        this.updateDom();
+    },
+    timerComplete: function () {
+        this.started = false;
+        this.fillerHeight = 0;
     }
 };
-window.onload = function(){
-  pomodoro.init();
+window.onload = function () {
+    pomodoro.init();
 };
 
 const form = document.getElementById("taskform");
@@ -94,83 +94,83 @@ var completionTimeInput = document.getElementById("completionTimeInput");
 var estimatedTimeInput = document.getElementById("estimatedTimeInput");
 var priorityInput = document.getElementById("priorityInput");
 
-form.addEventListener("submit", function(event){
-  event.preventDefault();
-  let task = taskInput.value;
-  let dueDate = dueDateInput.value;
-  let completionTime = completionTimeInput.value;
-  let estimatedTime = estimatedTimeInput.value;
-  let priorityRating = priorityInput.options[priorityInput.selectedIndex].value;
-  addTask(task, dueDate, estimatedTime, priorityRating, completionTime, false);
-  console.log(taskList);
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let task = taskInput.value;
+    let dueDate = dueDateInput.value;
+    let completionTime = completionTimeInput.value;
+    let estimatedTime = estimatedTimeInput.value;
+    let priorityRating = priorityInput.options[priorityInput.selectedIndex].value;
+    addTask(task, dueDate, estimatedTime, priorityRating, completionTime, false);
+    console.log(taskList);
 })
 
 var taskListArray = [];
 
 function addTask(taskDescription, dueDate, estimatedTime, priorityRating, completionTime, completionStatus) {
-  let d = new Date();
-  let dateCreated = d.getFullYear();
-  let task = {
-    taskDescription,
-    dueDate,
-    dateCreated,
-    estimatedTime,
-    completionTime,
-    priorityRating,
-    estimatedTime,
-    completionStatus
-  };
-  taskListArray.push(task);
-  renderTask(task);
+    let d = new Date();
+    let dateCreated = d.getFullYear();
+    let task = {
+        taskDescription,
+        dueDate,
+        dateCreated,
+        estimatedTime,
+        completionTime,
+        priorityRating,
+        estimatedTime,
+        completionStatus
+    };
+    taskListArray.push(task);
+    renderTask(task);
 }
 
-function renderTask(task){
-  // Create HTML elements
-  let item = document.createElement("li");
-  item.innerHTML = "<p>" + task.taskDescription + "</p>";
+function renderTask(task) {
+    // Create HTML elements
+    let item = document.createElement("li");
+    item.innerHTML = "<p>" + task.taskDescription + "</p>";
 
-  tasklist.appendChild(item);
+    tasklist.appendChild(item);
 
-  // Extra Task DOM elements
-  let delButton = document.createElement("button");
-  let delButtonText = document.createTextNode("Delete Task");
-  delButton.appendChild(delButtonText);
-  item.appendChild(delButton);
-
-
-  // Event Listeners for DOM elements
-  delButton.addEventListener("click", function(event){
-    event.preventDefault();
-    item.remove();
-  })
+    // Extra Task DOM elements
+    let delButton = document.createElement("button");
+    let delButtonText = document.createTextNode("Delete Task");
+    delButton.appendChild(delButtonText);
+    item.appendChild(delButton);
 
 
-  // Clear the input form
-  form.reset();
+    // Event Listeners for DOM elements
+    delButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        item.remove();
+    })
+
+
+    // Clear the input form
+    form.reset();
 }
 
 const wrapper = document.querySelector(".wrapper"),
-searchInput = wrapper.querySelector("input"),
-volume = wrapper.querySelector(".word i"),
-infoText = wrapper.querySelector(".info-text"),
-synonyms = wrapper.querySelector(".synonyms .list"),
-removeIcon = wrapper.querySelector(".search span");
+    searchInput = wrapper.querySelector("input"),
+    volume = wrapper.querySelector(".word i"),
+    infoText = wrapper.querySelector(".info-text"),
+    synonyms = wrapper.querySelector(".synonyms .list"),
+    removeIcon = wrapper.querySelector(".search span");
 let audio;
-function data(result, word){
-    if(result.title){
+function data(result, word) {
+    if (result.title) {
         infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please, try to search for another word.`;
-    }else{
+    } else {
         wrapper.classList.add("active");
         let definitions = result[0].meanings[0].definitions[0],
-        phontetics = `${result[0].meanings[0].partOfSpeech}  /${result[0].phonetics[0].text}/`;
+            phontetics = `${result[0].meanings[0].partOfSpeech}  /${result[0].phonetics[0].text}/`;
         document.querySelector(".word p").innerText = result[0].word;
         document.querySelector(".word span").innerText = phontetics;
         document.querySelector(".meaning span").innerText = definitions.definition;
         document.querySelector(".example span").innerText = definitions.example;
         audio = new Audio("https:" + result[0].phonetics[0].audio);
-        if(definitions.synonyms[0] == undefined){
+        if (definitions.synonyms[0] == undefined) {
             synonyms.parentElement.style.display = "none";
-        }else{
+        } else {
             synonyms.parentElement.style.display = "block";
             synonyms.innerHTML = "";
             for (let i = 0; i < 5; i++) {
@@ -181,37 +181,39 @@ function data(result, word){
         }
     }
 }
-function search(word){
+function search(word) {
     fetchApi(word);
     searchInput.value = word;
 }
-function fetchApi(word){
+function fetchApi(word) {
     wrapper.classList.remove("active");
     infoText.style.color = "#000";
     infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
     let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-    fetch(url).then(response => response.json()).then(result => data(result, word)).catch(() =>{
+    fetch(url).then(response => response.json()).then(result => data(result, word)).catch(() => {
         infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please, try to search for another word.`;
     });
 }
-searchInput.addEventListener("keyup", e =>{
+searchInput.addEventListener("keyup", e => {
     let word = e.target.value.replace(/\s+/g, ' ');
-    if(e.key == "Enter" && word){
+    if (e.key == "Enter" && word) {
         fetchApi(word);
     }
 });
-volume.addEventListener("click", ()=>{
+volume.addEventListener("click", () => {
     volume.style.color = "#4D59FB";
     audio.play();
-    setTimeout(() =>{
+    setTimeout(() => {
         volume.style.color = "#999";
     }, 800);
 });
-removeIcon.addEventListener("click", ()=>{
+removeIcon.addEventListener("click", () => {
     searchInput.value = "";
     searchInput.focus();
     wrapper.classList.remove("active");
     infoText.style.color = "#9A9A9A";
     infoText.innerHTML = "Type any existing word and press enter to get meaning, example, synonyms, etc.";
 });
-  
+
+
+
